c4f1506a9291a0b11ecaf3d0a10be7f24375b91d
Shauryasoni262 <92031162+Shauryasoni262@users.noreply.github.com>
2023-05-02 21:42:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("rsvp").addEventListener("click",First=()=>{
+    document.querySelector("#popup").style.display="flex";
+});
+
+document.getElementById("button1").addEventListener("click",second=()=>{
+    document.querySelector("#popup").style.display="none";
+})
+
+document.getElementById("button2").addEventListener("click",second=()=>{
+    document.querySelector("#popup").style.display="none";
+})
